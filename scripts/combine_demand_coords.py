24fa9522203db87e3b59be1e7f7a4e882c09e85e
Mats <zcecsai@ucl.ac.uk>
2025-06-25 21:04:23 +0100

    assigned GSP coordinates to demand, need to manually assign rest

--- scripts/combine_demand_coords.py
+++ scripts/combine_demand_coords.py
@@ -6,12 +6,14 @@ coords_df = pd.read_csv(snakemake.input["coords"])
 demand_key = "GSP"
 coords_key = "GSP ID"   
 
+# Used to check which ones missing from GSP coordinates list. See final comment.
 unmatched_ids = set(demand_df[demand_key]) - set(coords_df[coords_key])
-print(f'\nNUMBER OF UNMATCHED GSPS: {len(unmatched_ids)}')
 # if unmatched_ids:
-#     print(f"WARNING: {len(unmatched_ids)} GSP IDs in demand data not found in coordinates:")
-#     for uid in unmatched_ids:
-#         print(f"  - {uid}")
+#     print(f"\n Found {len(unmatched_ids)} unmatched GSP IDs in demand data (not found in coordinates):\n")
+#     for gsp_id in sorted(unmatched_ids):
+#         print(f"  - {gsp_id}")
+# else:
+#     print("All GSP IDs in demand data have corresponding entries in coordinates.")
 
 merged_df = pd.merge(
     demand_df,
@@ -21,5 +23,11 @@ merged_df = pd.merge(
     right_on=coords_key
 )
 
+# Remove empty columns
+merged_df = merged_df.loc[:, ~merged_df.columns.str.contains('^Unnamed')]
+merged_df = merged_df.dropna(axis=1, how='all')
 
 merged_df.to_csv(snakemake.output[0], index=False)
+
+# After this still missing ~70 GSPs, to be added by using ETYS2017 FLOP zones available in Appendix
+# A. Need to manually check minor FLOP zone on FES regional 'MAIN DATA' sheet.
--- scripts/extract_GSP_demand_peaks.py
+++ scripts/extract_GSP_demand_peaks.py
@@ -6,13 +6,18 @@ df = pd.read_excel(snakemake.input[0], sheet_name="Active", header=None)
 # skip empty rows
 df = df.iloc[2:]
 
-# keep headers if needed later (otherwise can remove)
+# keep headers for further processing
 df.columns = df.iloc[0]
 df = df[1:]
 
-# remove non-HT, after 2030.
+# remove non-HT, data after 2030.
 df = df[df.iloc[:, 0].astype(str).str.contains("HT")]
 df = df[pd.to_numeric(df.iloc[:, 6], errors='coerce') < 31]
 df = df[df.iloc[:, 2].notna() & (df.iloc[:, 2].astype(str).str.strip() != "")] # check if empty
+
+# remove GSPs where no demand estimated before 2030 (often no location info)
+df["DemandPk"] = pd.to_numeric(df["DemandPk"], errors="coerce")
+non_zero_gsps = df.groupby(df.columns[1])["DemandPk"].transform("sum") > 0
+df = df[non_zero_gsps]
 
 df.to_csv(snakemake.output[0], index=False)
